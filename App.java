801ee73bf33711b901ba0b815470da9161f7a706
VidadiMurad <vidad0murad@gmail.com>
2026-04-18 10:19:31 +0400

    Add Variables

--- App.java
+++ App.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+
+        // Variables
+        double weight;
+        double height;
+        double resultOp;
+
         
+
+
+
+        scanner.close();
 
     }
 }
